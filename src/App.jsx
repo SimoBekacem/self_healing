cc3c4ce389ce34db6c686fac10815cbcf45f889b
Mohamed Belkacem <125382745+SimoBekacem@users.noreply.github.com>
2025-05-28 15:36:33 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+// hello this is a new code
 function App() {
   const [count, setCount] = useState(0)
 
